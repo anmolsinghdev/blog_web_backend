dca4f2c96d4a23525e851cc9e7a36073cacb6a32
Anmol Singh <anmols@chetu.com>
2022-06-13 22:14:49 +0530

    nothing

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const cors = require('cors');
 const app = express();
 require('./connections/connect');
-const port = 4000;
+require('dotenv').config();
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 const userRoutes = require("./routes/user");
+
 app.use(userRoutes);
 
 app.use((req, res) => {
@@ -16,4 +17,4 @@ app.use((req, res) => {
     });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(process.env.port, () => console.log(`Example app listening on port ${process.env.port}!`));
